069503c54414f7b5fa920c710ef6266aee2b59da
Zhehai Zhang <zhehaizhang1@gmail.com>
2020-12-27 17:18:07 -0500

    Stuff

--- nodeutils.c
+++ nodeutils.c
@@ -7,12 +7,13 @@ Tree*
 make_tree(Index* index)
 {
     Tree* root;
-
     root = malloc(sizeof(Tree));
-    root->nodeType = tree;
+    root->nodeType = NodeType_tree;
     root->name = "Root folder lmao";
     root->children = malloc(0);
     root->cnum = 0;
+
+    struct stat sb;
 
     // Making the node
     Tree* new_node;
@@ -21,43 +22,53 @@ make_tree(Index* index)
     
     for (int i = 0; i < index->index_length; i++) {
         IndexItem current = index->index_items[i];
-        char *ptr = strtok(current.file_path,"/");
+        char *nextFolder = strtok(current.file_path,"/");
+        char *path = malloc(strlen(current.filepath)*sizeof(char))
 
         // Node we're adding to tree
         new_node = malloc(sizeof(Tree));
         new_node->hash = current.hash;
-        new_node->nodeType = blob;
+        new_node->nodeType = NodeType_blob;
         new_node->name = current.file_path;
         new_node->children = malloc(0);
         new_node->cnum = 0;
 
-        add_to(root, new_node, ptr);
+        add_to(root, new_node, nextFolder,path);
+        free(path);
     }
 
     return root;
 }
 
 void
-add_to(Tree* root, Tree* new_node, char* path)
+add_to(Tree* root, Tree* new_node, char* nextFolder,char* path)
 {
-    /* if (ptr!=NULL){ // If still going to path */
-    /*     // Check if folder exists. If it does, then go in. */
+    if (nextFolder!=NULL) { // If still going to path 
+        strcat(path,nextFolder);
+        strcat(path,"/");
+        // If folder doesn't exist, make it
+        int folderpos = find_folder(root,path);
+        if (folderpos == -1) {
+            
 
-    /*     // Otherwise, make the folder and go in */
 
+        }
+        //Go into folder
+        else {
 
-
-
-    /*     ptr = strtok(NULL,"/"); */
-    /* } */
-    /* else{//We've reached the end, add the object file here. */ 
-    /*     root->cnum+=1; */
-    /*     root->children = realloc(root->children,root->cnum); */
-    /*     root->children[root->cnum-1] = &new_node;//Set it equal */
-
-    /* } */
+        }
+        nextFolder = strtok(NULL,"/");
+    }
+    else {//We've reached the end, add the object file here. 
+        root->cnum+=1;
+        root->children = realloc(root->children,root->cnum);
+        root->children[root->cnum-1] = &new_node;//Set it equal
+    }
 }
-/*
+
 int
-find_folder(,Tree root)
-*/
+find_folder(Tree* root, char* path)
+{
+    return 0;
+}
+
